fb6484cf28b381035447d6a80d7cb731b587f5da
Lean <80percentlean@gmail.com>
2026-05-13 13:19:31 -0700

    Implement API error handling

--- src/api/events.ts
+++ src/api/events.ts
@@ -1,5 +1,6 @@
 import { $guilds$_$scheduledevents, createRest } from 'discord-hono'
 import { Hono } from 'hono'
+import { HTTPException } from 'hono/http-exception'
 
 export const events = new Hono<{
   Bindings: CloudflareBindings & {
@@ -15,14 +16,26 @@ export const events = new Hono<{
 events.get('/', async (c) => {
   const rest = createRest(c.env.DISCORD_TOKEN)
 
-  const events = await rest('GET', $guilds$_$scheduledevents, [
+  const eventsRes = await rest('GET', $guilds$_$scheduledevents, [
     c.env.DISCORD_TEST_GUILD_ID,
-  ]).then((res) => res.json())
+  ])
+
+  const eventsJson = await eventsRes.json()
+  if (!eventsRes.ok) {
+    throw new HTTPException(502, {
+      message: 'Encountered a Discord API error',
+      cause: {
+        status: eventsRes.status,
+        statusText: eventsRes.statusText,
+        data: eventsJson,
+      },
+    })
+  }
 
   // TODO: remove unnecessary data from events
 
   return c.json({
-    data: events,
+    data: eventsJson,
     meta: {
       apiVersion: 'v1',
       success: true,
